be01152b441e139a8a71b9f6f8abc8b62713898d
Andrew Martins <andrew_omartins@yahoo.com>
2025-02-03 23:04:26 -0500

    done with promises

--- asyncPromise.js
+++ asyncPromise.js
@@ -1,6 +1,26 @@
+const fetchMe = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+        resolve("AHHHH")
+    }, 3000)
+    }) 
+    return promise
+}
+
+
 setTimeout(() => {
     console.log("look at me!!!!")
-}, 2000);
+    fetchMe()
+    .then(text => {
+        console.log(text)
+        return fetchMe()
+    })
+    .then(text2 => {
+        console.log(text2)
+    })
+}, 3300);
+
+
 
 console.log("sup slut")
 console.log("yooooo")
