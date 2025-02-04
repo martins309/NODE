771e6441f1c49b94dcaef3e82eb817a6bc55ce76
Andrew Martins <andrew_omartins@yahoo.com>
2025-02-03 22:41:18 -0500

    messing with the spread operator

--- spreadRest.js
+++ spreadRest.js
@@ -1,16 +1,23 @@
 const array = [1,2,3,4,5]
 
 
- function spread () {
-    console.log([...array])
+const spread = () => {
 
 }
-console.log(spread())
-
-
 
 const object = {
     name: "chicken",
     age: 666,
     power: "frying chicken with my nails"
 }
+
+//the spread can copy objects
+const copied = {...object}
+console.log(copied)
+
+const rest = (...args) => {
+    if(args.length % 2 !== 0) {
+        console.log(Math.pow(args))
+    }
+}
+console.log(rest([1,2,3,43,45,6]))
