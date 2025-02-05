c1e8f3d384a063b13f8ce4b525c9c2ff370fb46a
Andrew Martins <andrew_omartins@yahoo.com>
2025-02-04 23:04:43 -0500

    .

--- reqRes.js
+++ reqRes.js
@@ -1,4 +1,6 @@
 const http = require('http')
+const fs = require('fs')
+
 
 //creating a web server that has a different response depending on the url
 const server = http.createServer((req, res) => {
@@ -6,16 +8,25 @@ const server = http.createServer((req, res) => {
 
     //parsing the url
     const url = req.url
-
+    const method = req.method
     //returns a response that gives the user an input form and a button that 
     // will send a new request and return 
+    //return will quit the function
     if(url === '/') {
         res.write('<html>')
         res.write('<head><title>Enter Message </title><head>')
         res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send It</button></form></body>')
         res.write("'</html>")
-      }
-    
+        return res.end()
+    }
+    //when the user submits their information "POST", it will write and file on the server
+    //check out res.writeHead()
+if(url === '/message' && method === 'POST'){
+    fs.writeFileSync('message.txt', "Lookat me")
+        res.statusCode = 302
+        res.setHeader('Location', '/')
+        return res.end()
+}
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
     res.write('<head><title>Node Page </title><head>')
