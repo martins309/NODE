aa747479f4592d72cdd4de6597949867c41d7df0
Andrew Martins <andrew_omartins@yahoo.com>
2025-02-04 22:26:01 -0500

    wrote html to display in browser

--- reqRes.js
+++ reqRes.js
@@ -4,7 +4,10 @@ const server = http.createServer((req, res) => {
     console.log(req.url, req.headers, req.method)
     
     res.setHeader('Content-Type', 'text/html')
-    res.write('<head><head>')
+    res.write('<html>')
+    res.write('<head><title>This is a page that i wrote in node</title><head>')
+    res.write('<body></body>')
+    res.write("'</html>")
 })
 
 server.listen(3000)
