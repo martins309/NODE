6edaf371282d20e1b4e5d2c19677628fb0359cd4
Andrew Martins <andrew_omartins@yahoo.com>
2025-02-04 22:35:20 -0500

    creating a form for the user to submit to

--- reqRes.js
+++ reqRes.js
@@ -1,12 +1,18 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.headers, req.method)
+      const url = req.url
+      if(url === '/') {
+        res.write('<html>')
+        res.write('<head><title>Enter Message </title><head>')
+        res.write('<body><form action="/message"><input type="text"><button type="submit">Send It</button></form></body>')
+        res.write("'</html>")
+      }
     
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
-    res.write('<head><title>This is a page that i wrote in node</title><head>')
-    res.write('<body></body>')
+    res.write('<head><title>Node Page </title><head>')
+    res.write('<body><h1>This is a stick up!</h1></body>')
     res.write("'</html>")
 })
 
