1ed462bff69a6b5384e2ed0948008caf34d02b3b
Andrew Martins <andrew_omartins@yahoo.com>
2025-02-04 22:42:20 -0500

    comments with explainations

--- reqRes.js
+++ reqRes.js
@@ -1,11 +1,18 @@
 const http = require('http')
 
+//creating a web server that has a different response depending on the url
 const server = http.createServer((req, res) => {
-      const url = req.url
-      if(url === '/') {
+
+
+    //parsing the url
+    const url = req.url
+
+    //returns a response that gives the user an input form and a button that 
+    // will send a new request and return 
+    if(url === '/') {
         res.write('<html>')
         res.write('<head><title>Enter Message </title><head>')
-        res.write('<body><form action="/message"><input type="text"><button type="submit">Send It</button></form></body>')
+        res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send It</button></form></body>')
         res.write("'</html>")
       }
     
