de0562faeb9d1dbecd2172bb7342546778af4551
Andrew Martins <andrew_omartins@yahoo.com>
2025-02-04 22:24:09 -0500

    writing to the request

--- reqRes.js
+++ reqRes.js
@@ -2,6 +2,9 @@ const http = require('http')
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.headers, req.method)
+    
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<head><head>')
 })
 
 server.listen(3000)
